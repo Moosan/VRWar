94f62304102b7b303c7dc0ee540b737fa3216b13
Moosan <washer2016@outlook.jp>
2019-05-05 08:35:25 +0900

    iroiro

---
+++ Assets/Scripts/VRSystem/SteamVRSystem/HandAttachment.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using Valve.VR;
+public abstract class HandAttachment : MonoBehaviour
+{
+    [SerializeField]//なんのボタンを受け取るかをインスペクターで決めれる
+    private SteamVR_Action_Boolean actionBool;
+    public void OnTouch()
+    {
+        
+    }
+}
